666ee067b3d52dec4c7664b49ca5d7e740fc7c6b
Alonso Matos <alonsoaman@hotmail.com>
2019-10-26 11:11:47 -0300

    agora funciona, yuri

--- backend/bnana/admin.py
+++ backend/bnana/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from bnana.models.bananasuser import BananasUser
 
-from bnana.models import Estabelecimento, Exame, Maquina, Orientador, Pessoa
+# from bnana.models import Estabelecimento, Exame, Maquina, Orientador, Pessoa
 
-# Register your models here.
-admin.site.register(Estabelecimento)
-admin.site.register(Exame)
-admin.site.register(Maquina)
-admin.site.register(Orientador)
-admin.site.register(Pessoa)
+# # Register your models here.
+# admin.site.register(Estabelecimento)
+# admin.site.register(Exame)
+# admin.site.register(Maquina)
+# admin.site.register(Orientador)
+# admin.site.register(Pessoa)
 
 
 class BananasUserAdmin(UserAdmin):
--- backend/bnana/models/__init__.py
+++ backend/bnana/models/__init__.py
@@ -1,9 +1,9 @@
 from bnana.models.bananasuser import BananasUser
-from bnana.models.estabelecimento import Estabelecimento
-from bnana.models.maquina import Maquina, MaquinaTipo
-from bnana.models.orientador import Orientador
-from bnana.models.pessoa import Pessoa
-from bnana.models.exame import Exame
+# from bnana.models.estabelecimento import Estabelecimento
+# from bnana.models.maquina import Maquina, MaquinaTipo
+# from bnana.models.orientador import Orientador
+# from bnana.models.pessoa import Pessoa
+# from bnana.models.exame import Exame
 
 
 
